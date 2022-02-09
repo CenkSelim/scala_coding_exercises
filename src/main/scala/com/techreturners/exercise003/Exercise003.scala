ab3a0fce376183864ed20268ef4ea112301719c7
Cenk <muzzyathome@gmail.com>
2022-02-09 16:23:31 +0000

    iceCreamFlavour succint code

--- src/main/scala/com/techreturners/exercise003/Exercise003.scala
+++ src/main/scala/com/techreturners/exercise003/Exercise003.scala
@@ -1,10 +1,16 @@
 package com.techreturners.exercise003
 
 class Exercise003 {
+  
+  val flavours = Array("Pistachio",
+    "Raspberry Ripple",
+    "Vanilla",
+    "Mint Chocolate Chip",
+    "Chocolate",
+    "Mango Sorbet"
+  )
 
-  def getIceCreamCode(iceCreamFlavour: String): Int = {
-    throw new UnsupportedOperationException("You can delete this line and add your code here.")
-  }
+  def getIceCreamCode(iceCreamFlavour: String): Int = flavours.indexWhere( _ == iceCreamFlavour)
 
   def iceCreamFlavours: Array[String] = {
     throw new UnsupportedOperationException("You can delete this line and add your code here.")
