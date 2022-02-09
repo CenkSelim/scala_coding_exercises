4b6b363da6b7c94a14a03bb6982e56fe72560fe5
Cenk <muzzyathome@gmail.com>
2022-02-09 15:44:49 +0000

    canWatchFilm succint code

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -4,6 +4,6 @@ class Exercise002 {
 
   def isFromManchester(person: Person): Boolean = if (person.city == "Manchester")  true else false
 
-  def canWatchFilm(person: Person, ageLimit: Int): Boolean = false
+  def canWatchFilm(person: Person, ageLimit: Int): Boolean = if (person.age >= ageLimit) true else false
 
 }
