d3b3717684886a88b57cada8af3f6bc6bd8a1d93
Cenk <muzzyathome@gmail.com>
2022-02-09 15:42:39 +0000

    isFromManchester succint code

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -2,7 +2,7 @@ package com.techreturners.exercise002
 
 class Exercise002 {
 
-  def isFromManchester(person: Person): Boolean = false
+  def isFromManchester(person: Person): Boolean = if (person.city == "Manchester")  true else false
 
   def canWatchFilm(person: Person, ageLimit: Int): Boolean = false
 
