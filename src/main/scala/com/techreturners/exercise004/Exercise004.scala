56de5af4d3b6749ab535e48afea0d2ea30dfc9c4
Cenk <muzzyathome@gmail.com>
2022-02-14 11:46:25 +0000

    GigaSecond magic number added to code implemented in exercise004

--- src/main/scala/com/techreturners/exercise004/Exercise004.scala
+++ src/main/scala/com/techreturners/exercise004/Exercise004.scala
@@ -4,7 +4,9 @@ import java.time.{LocalDate, LocalDateTime}
 
 class Exercise004(var dateTime: LocalDateTime) {
 
+  val GigaSecond: Int  = 1000000000;
+
   def this(date: LocalDate) = this(date.atStartOfDay())
 
-  def getDateTimeWithGigaSecond: LocalDateTime = this.dateTime.plusSeconds(1000000000)
+  def getDateTimeWithGigaSecond: LocalDateTime = this.dateTime.plusSeconds(GigaSecond)
 }
